c93a0a0e4833d7192b4a41bf5e242232f6b23564
Geetesh <125589203+geetesh2004@users.noreply.github.com>
2025-09-05 21:00:16 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -0,0 +1,17 @@
+// Last updated: 9/5/2025, 9:00:15 PM
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
